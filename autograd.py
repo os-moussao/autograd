01f28523a3826b1a87c0cc194d6eda6ca97083a6
Oussama Moussaoui <omoussaoui1337@gmail.com>
2026-04-02 18:58:03 +0100

    chore: add Grad.clip

--- autograd.py
+++ autograd.py
@@ -216,3 +216,33 @@ class Grad():
         x = x.flatten()
         assert len(x) == 1, "Only one element allowed"
         return x[0]
+
+    @staticmethod
+    def _clip(x: Grad, min_val, max_val) -> Grad:
+        out = Grad(
+            val=min(max(x.val, min_val), max_val),
+            required_grad=x.requires_grad,
+            children=(x,)
+        )
+
+        def out_backward():
+            if x.requires_grad:
+                x.grad += (min_val <= x.val <= max_val) * out.grad
+
+        out._backward = out_backward
+
+        return out
+
+    @staticmethod
+    @overload
+    def clip(x: Grad, min_val, max_val) -> Grad: ...
+
+    @staticmethod
+    @overload
+    def clip(x: np.ndarray, min_val, max_val) -> np.ndarray: ...
+
+    @staticmethod
+    def clip(x: Grad | np.ndarray, min_val, max_val) -> Grad | np.ndarray:
+        if isinstance(x, Grad):
+            return Grad._clip(x, min_val, max_val)
+        return np.vectorize(lambda x: Grad._clip(x, min_val, max_val))(x)
